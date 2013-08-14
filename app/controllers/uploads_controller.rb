47463379745aa32c568a6b05978f37abb257bc24
Matt Andreko <mandreko@gmail.com>
2013-08-14 11:18:57 -0400

    initial commit

---
+++ app/controllers/uploads_controller.rb
@@ -0,0 +1,103 @@
+require 'fileutils'
+
+class UploadsController < ApplicationController
+  # GET /uploads
+  # GET /uploads.json
+  def index
+    @uploads = Upload.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @uploads }
+    end
+  end
+
+  # GET /uploads/1
+  # GET /uploads/1.json
+  def show
+    @upload = Upload.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @upload }
+    end
+  end
+
+  # GET /uploads/new
+  # GET /uploads/new.json
+  def new
+    @upload = Upload.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @upload }
+    end
+  end
+
+  # GET /uploads/1/edit
+  def edit
+    @upload = Upload.find(params[:id])
+  end
+
+  # POST /uploads
+  # POST /uploads.json
+  def create
+
+    #Create new upload object
+    @upload = Upload.new()
+    #Set filename to the original filename provided by user
+    @upload.filename = params[:upload][:filename].original_filename
+
+    #Get pointer to the uploaded temp file
+    tmp = params[:upload][:filename].tempfile
+    #Create filename in 'public' folder for upload
+    file = File.join("public", @upload.filename)
+    #Copy file to 'public' folder
+    FileUtils.cp tmp.path, file
+
+    ##
+    # Do file processing here. Or even don't copy to 'public' folder, just create a delayed_job
+    # to process file with a tie to the Upload activemodel
+    ##
+
+    #FileUtils.rm file
+
+    respond_to do |format|
+      if @upload.save
+        format.html { redirect_to @upload, notice: 'Upload was successfully created.' }
+        format.json { render json: @upload, status: :created, location: @upload }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @upload.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /uploads/1
+  # PUT /uploads/1.json
+  def update
+    @upload = Upload.find(params[:id])
+
+    respond_to do |format|
+      if @upload.update_attributes(params[:upload])
+        format.html { redirect_to @upload, notice: 'Upload was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @upload.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /uploads/1
+  # DELETE /uploads/1.json
+  def destroy
+    @upload = Upload.find(params[:id])
+    @upload.destroy
+
+    respond_to do |format|
+      format.html { redirect_to uploads_url }
+      format.json { head :no_content }
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestUpload::Application.config.secret_token = '099e61912e546c38f63da480f40a6955a891366ccbc383c2b20006683cb3c55b7fa3d62684ebd6edc6b460c42926b20677f9c74cd5f247ef4791bb69c37a7342'
